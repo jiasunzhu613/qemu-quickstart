ef12926bcd1be86b1f505ce804b241e80bcef869
jonathanzhu <jiasunzhu@gmail.com>
2025-12-15 23:39:59 -0500

    rm random files

--- run_alpine.sh
+++
@@ -1,5 +0,0 @@
-qemu-system-x86_64 \ 
-    -boot d \
-    -m 2048 \
-    -nic user,model=virtio \
-    alpine.qcow2
--- start_alpine.sh
+++
@@ -1,9 +0,0 @@
-qemu-system-x86_64 \
-    -cdrom alpine-standard-3.8.0-x86_64.iso \
-    -boot d \
-    -m 2048 \
-    -nic user,model=virtio \
-    alpine.qcow2
-
-# TODO: this does not have network to get internet
-# Use -nic option
