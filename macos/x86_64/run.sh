f1669b99124467bbc33b43fffc470427da4c2d7d
jonathanzhu <jiasunzhu@gmail.com>
2025-12-18 19:53:44 -0500

    change to nographics for runscript

--- macos/x86_64/run.sh
+++ macos/x86_64/run.sh
@@ -32,5 +32,6 @@ qemu-system-x86_64 \
     -m 4096 \
     -hda "$QCOW2_IMAGE" \
     -device e1000,netdev=net0 \
-    -netdev user,id=net0,hostfwd=tcp::"$SSH_PORT"-:22
+    -netdev user,id=net0,hostfwd=tcp::"$SSH_PORT"-:22 \
+    -nographic
     # -boot c
